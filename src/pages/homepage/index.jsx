63caa92c429b3733983ce1b2bf59b1700f048283
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2020-04-29 23:30:40 +0200

    feat: added styling, menu and directory component

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./HomePage.scss";
+import { Directory } from "../../components";
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
